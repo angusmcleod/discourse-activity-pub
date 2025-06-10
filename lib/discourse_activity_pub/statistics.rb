293a81cccd0422b721b5eeae7650b18c9877e309
Angus McLeod <angus@mcleod.org.au>
2025-06-10 09:31:10 +0100

    Update Statistics#nodeinfo users method to local_users

--- lib/discourse_activity_pub/statistics.rb
+++ lib/discourse_activity_pub/statistics.rb
@@ -4,15 +4,15 @@ module DiscourseActivityPub
     # https://github.com/jhass/nodeinfo/blob/main/schemas/2.2/schema.json#usage
     def nodeinfo
       {
-        users_total: active_users.count,
-        users_seen_half_year: active_users.where("last_seen_at > ?", 180.days.ago).count,
-        users_seen_month: active_users.where("last_seen_at > ?", 30.days.ago).count,
+        users_total: local_users.count,
+        users_seen_half_year: local_users.where("last_seen_at > ?", 180.days.ago).count,
+        users_seen_month: local_users.where("last_seen_at > ?", 30.days.ago).count,
         posts_local: local_posts.where("reply_to_post_number IS NULL").count,
         replies_local: local_posts.where("reply_to_post_number IS NOT NULL").count,
       }
     end
 
-    def active_users
+    def local_users
       valid_users.where("staged IS FALSE")
     end
 
